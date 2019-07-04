a9d6e3b49e451c0e88f8f3fabe70692a0c324799
Robert Salmi <Robert.salmi@hotmail.com>
2019-07-04 15:33:42 +0300

    3.4, 3.5, 3.6, 3.7, 3.8 tehtävät valmiina

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+const bodyParser = require("body-parser");
+
+morgan.token("postData", function(req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms  :postData"
+  )
+);
+
+app.use(bodyParser.json());
 
 let persons = [
   {
-    name: "Esko Testaaja",
+    name: "Yah Mon",
     number: "050-35035350",
     id: 1
   },
@@ -46,6 +60,49 @@ app.get("/info", (req, res) => {
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
+});
+
+const generateId = () => {
+  const random = Math.floor(Math.random() * 99999);
+  return random;
+};
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: "name missing"
+    });
+  }
+  if (!body.number) {
+    return res.status(400).json({
+      error: "number missing"
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  };
+  const checkNames = persons.map(person => person.name).includes(person.name);
+
+  if (checkNames === true) {
+    return res.status(406).json({
+      error: "name must be unique"
+    });
+  } else {
+    persons = persons.concat(person);
+    res.json(person);
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  res.status(204).end();
 });
 
 const port = 3001;
